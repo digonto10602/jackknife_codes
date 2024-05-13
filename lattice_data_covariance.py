b721248207b9d37d604690adac2601cc8368906f
Md Habib E Islam <digonto10602@gmail.com>
2024-05-13 09:34:05 -0400

    dont know what i fixed

--- lattice_data_covariance.py
+++ lattice_data_covariance.py
@@ -16,6 +16,15 @@ sys.path.insert(1, '/home/digonto/Codes/Practical_Lattice_v2/jackknife_codes/')
 
 import jackknife 
 
+def E_to_Ecm(En, P):
+    return np.sqrt(En**2 - P**2)
+
+def Esq_to_Ecmsq(En, P):
+    return En**2 - P**2
+
+def Ecmsq_to_Esq(Ecm, P):
+    return Ecm**2 + P**2
+
 #we build the covariance between different states 
 #we consider that all the files have same length
 #we return the states with their errors as well
@@ -29,6 +38,12 @@ def covariance_between_states_L20(energy_cutoff, list_of_mom):
     state_file_list = []
     nP_list = []
 
+    xi = 3.444
+    L = 20
+    Lbyas = L*xi 
+    pi = math.acos(-1.0) 
+    twopibyLbyas = 2.0*pi/Lbyas 
+
     for moms in list_of_mom:
         for states in range(max_state_num):
             filename = path_to_files + 'mass_' + moms + '_state' + str(states) + '.jack'
@@ -38,34 +53,42 @@ def covariance_between_states_L20(energy_cutoff, list_of_mom):
                 (temp0, datatemp) = np.genfromtxt(filename, unpack=True, skip_header=1)
                 resampled_datatemp = jackknife.jackknife_resampling(datatemp)
                 avgtemp = jackknife.jackknife_average(resampled_datatemp)
-                if(avgtemp<energy_cutoff):
+                if(moms=='000_A1m'):
+                    nPx = 0
+                    nPy = 0
+                    nPz = 0
+                    #nP_list.append([nPx,nPy,nPz])
+                elif(moms=='100_A2'):
+                    nPx = 1
+                    nPy = 0
+                    nPz = 0
+                    #nP_list.append([nPx,nPy,nPz])
+                elif(moms=='110_A2'):
+                    nPx = 1
+                    nPy = 1
+                    nPz = 0
+                    #nP_list.append([nPx,nPy,nPz])
+                elif(moms=='111_A2'):
+                    nPx = 1
+                    nPy = 1
+                    nPz = 1
+                    #nP_list.append([nPx,nPy,nPz])
+                elif(moms=='200_A2'):
+                    nPx = 2
+                    nPy = 0
+                    nPz = 0
+                    #nP_list.append([nPx,nPy,nPz])  
+                Px = twopibyLbyas*nPx 
+                Py = twopibyLbyas*nPy 
+                Pz = twopibyLbyas*nPz 
+                P = np.sqrt(Px*Px + Py*Py + Pz*Pz)
+                avgtemp_Ecm = E_to_Ecm(avgtemp,P)  
+                
+                if(avgtemp_Ecm<energy_cutoff):
                     check_total_states = check_total_states + 1 
                     state_file_list.append(filename)
-                    if(moms=='000_A1m'):
-                        nPx = 0
-                        nPy = 0
-                        nPz = 0
-                        nP_list.append([nPx,nPy,nPz])
-                    elif(moms=='100_A2'):
-                        nPx = 1
-                        nPy = 0
-                        nPz = 0
-                        nP_list.append([nPx,nPy,nPz])
-                    elif(moms=='110_A2'):
-                        nPx = 1
-                        nPy = 1
-                        nPz = 0
-                        nP_list.append([nPx,nPy,nPz])
-                    elif(moms=='111_A2'):
-                        nPx = 1
-                        nPy = 1
-                        nPz = 1
-                        nP_list.append([nPx,nPy,nPz])
-                    elif(moms=='200_A2'):
-                        nPx = 2
-                        nPy = 0
-                        nPz = 0
-                        nP_list.append([nPx,nPy,nPz])    
+                    nP_list.append([nPx,nPy,nPz])
+                    
     
     state_no = []
     temp_state_no = 0
@@ -105,14 +128,38 @@ def covariance_between_states_L20(energy_cutoff, list_of_mom):
             (temp0, data1) = np.genfromtxt(file1, unpack=True, skip_header=1)
             (temp0, data2) = np.genfromtxt(file2, unpack=True, skip_header=1) 
 
+            nPx1 = nP_list[i][0]
+            nPy1 = nP_list[i][1]
+            nPz1 = nP_list[i][2]
+            Px1 = twopibyLbyas*nPx1 
+            Py1 = twopibyLbyas*nPy1 
+            Pz1 = twopibyLbyas*nPz1 
+            P1 = np.sqrt(Px1*Px1 + Py1*Py1 + Pz1*Pz1)
+            
+            nPx2 = nP_list[j][0]
+            nPy2 = nP_list[j][1]
+            nPz2 = nP_list[j][2]
+            Px2 = twopibyLbyas*nPx2 
+            Py2 = twopibyLbyas*nPy2 
+            Pz2 = twopibyLbyas*nPz2 
+            P2 = np.sqrt(Px2*Px2 + Py2*Py2 + Pz2*Pz2)
+
             resampled_data1 = jackknife.jackknife_resampling(data1)
             resampled_data2 = jackknife.jackknife_resampling(data2)
 
-            avg1 = jackknife.jackknife_average(resampled_data1)
-            avg2 = jackknife.jackknife_average(resampled_data2)
+            resampled_data1_ecm = np.zeros((len(resampled_data1)))
+            resampled_data2_ecm = np.zeros((len(resampled_data2)))
 
-            err1 = jackknife.jackknife_error(resampled_data1)
-            err2 = jackknife.jackknife_error(resampled_data2) 
+            for k in range(len(resampled_data1)):
+                resampled_data1_ecm[k] = E_to_Ecm(resampled_data1[k],P1)
+            for l in range(len(resampled_data2)):
+                resampled_data2_ecm[l] = E_to_Ecm(resampled_data1[l],P1)
+            
+            avg1 = jackknife.jackknife_average(resampled_data1_ecm)
+            avg2 = jackknife.jackknife_average(resampled_data2_ecm)
+
+            err1 = jackknife.jackknife_error(resampled_data1_ecm)
+            err2 = jackknife.jackknife_error(resampled_data2_ecm) 
 
             size = len(resampled_data1)
             n = size 
@@ -123,6 +170,9 @@ def covariance_between_states_L20(energy_cutoff, list_of_mom):
 
             covariance_matrix[i][j] =  ((n-1.0)/n)*sum_res 
 
+        
+
+        
         states_avg[i] = avg1 
         states_err[i] = err1   
 
